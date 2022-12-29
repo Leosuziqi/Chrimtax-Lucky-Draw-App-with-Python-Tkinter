1129c8fa267a1b03fef6436c1b3167fc593e551a
Leosuziqi <50681212+Leosuziqi@users.noreply.github.com>
2022-12-29 14:40:25 -0800

    Change Demand from User: 1
    Add an Entry in Window to input the number of Guests

--- lucky_draw.py
+++ lucky_draw.py
@@ -13,26 +13,25 @@ import pygame
 import keyboard
 
 size_result = 100
+
+#Main Window
 root = Tk()
 
 root.title('Christmas Dinner ')
 
 #refresh()
 
-
-
 #w = bg.width()
 w = root.winfo_screenwidth()
 h = root.winfo_screenheight()
 #h = bg.height()
 
-## set background
+## set background and Text
 root.overrideredirect(True)
 root.configure(background='white')
 image = Image.open('cxtree.jpg')
 new_image = image.resize((int(w*4/5), h))
 bg = ImageTk.PhotoImage(new_image)
-
 
 root.geometry('%dx%d' % (w,h))
 label = Label(root,image=bg)
@@ -43,11 +42,20 @@ Header.config(font=("Courier", 50))
 root.overrideredirect(True)
 Header.place(relx=0.9, rely=0.05, anchor="center")
 
-count = 0.1
-def refresh(event):
-    qty = 100
-    global count
+# Entry for guest_number and prize_number
+guest_number_label=tkinter.Label(root, text="# of Guests: ",font=("Ariel", int(30*w/1536),"bold"), bg="red")
+guest_number_label.place(relx=0.1, rely=0.35, anchor="s")
+guest_number_entry=tkinter.Entry(root,font=("Ariel", int(30*w/1536),"bold"), width=5)
+guest_number_entry.place(relx=0.1, rely=0.35, anchor="n")
 
+count = 0.1
+
+#Command when Click Start
+def refresh(event):
+    qty = guest_number
+    global count
+    
+    #Generate a random value
     number_new = random.randint(88208, 88208 + qty)
     num_str=str(0) + str(number_new)+'\n'
 
@@ -61,10 +69,8 @@ def refresh(event):
         print("re")
         number_new = random.randint(88208, 88208 + qty)
         num_str = str(0) + str(number_new) + '\n'
-
-
+        
     #    number = random.randint(88208, 88208 + qty)
-
     result = str(0) + str(number_new)
     output = result + '\n'
 
@@ -74,24 +80,18 @@ def refresh(event):
     f.write('{}'.format(output))
     f.close()
 
-
+    #Display Result
     winnter = tkinter.Label(root, text=result)
     winnter.config(font=("Courier", 60))
     winnter.place(relx=0.9, rely=0.05+count, anchor="center")
     count=count+0.09
     
-# Buttom
+# Start Buttom
 button_1 = Button(root, text="START",font=("Courier", 50),bg= 'grey',activebackground='green')
 button_1.place(relx=0.4, rely=0.75, anchor="center")
 button_1.bind("<Button-1>", refresh)
 root.bind("<Return>")
 root.bind("<Return>", refresh)
-
-button_2 = Button(root, text="Re-Draw",font=("Courier", 40),bg= 'grey',activebackground='green')
-button_2.place(relx=0.4, rely=0.9, anchor="center")
-button_2.bind("<Button-1>", retrial)
-root.bind("<Shift_R >")
-root.bind("<Shift_R >",retrial)
 
 ## adding bg music
 url = "Jingle-Bells-3.mp3"
